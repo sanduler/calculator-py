59a7b10248750106a074b08700d4520f489bf2d4
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-21 14:31:51 -0800

    finished all the needed operations

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from background import logo
 
 print(logo)
 
-# todo create add functionality
 
-# todo create subtract functionality
+def add(n1, n2):
+    """This function takes integers and returns a sum"""
+    return n1 + n2
 
-# todo create multiply functionality
 
-# todo create divide functionality
+def subtract(n1, n2):
+    """This functions takes integers and returns a difference"""
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    """This function takes integers and returns a product"""
+    return n1 * n2
+
+
+def divide(n1, n2):
+    """This function takes integers and returns a quotient"""
+    if n2 == 0:
+        return("Undefined")
+    else:
+        return n1 / n2
