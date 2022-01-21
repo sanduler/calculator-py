d155aa3d967e10b1c24ba4d727c59a57f8fe33e0
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-01-21 14:27:42 -0800

    created the to do for diffrent functions that need

---
+++ main.py
@@ -0,0 +1,11 @@
+from background import logo
+
+print(logo)
+
+# todo create add functionality
+
+# todo create subtract functionality
+
+# todo create multiply functionality
+
+# todo create divide functionality
